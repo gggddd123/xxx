1e1b11a9b03c11b50385ba1d7fd3754233e2f8b2
gggddd123 <40315187+gggddd123@users.noreply.github.com>
2018-12-30 14:00:33 +0000

    start

---
+++ Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+const Header = () => {
+    return (
+        <div>
+            <header>
+      <video className="video" autoPlay loop >
+        <video />
+      </video>
+      <tr>
+        <td><button className="renk1">Weekly Forecast</button></td>
+        <td><button className="renk1">Playlist</button></td>
+        <td><button className="renk1">My Mood Today</button></td>
+        <td><button className="renk">#NOFILTERWEATHER</button></td>
+      </tr>
+      </header>
+      </div>
+     );
+  } 
+  
+
+
+
+export default Header;
---
+++ Titles.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">#NOFILTER WEATHER</h1>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+	</div>
+);
+
+export default Titles;
